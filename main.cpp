b33ceedf419088512e7efba5e3f5479f0e1e4224
dessyacheampong <77683806+dessyacheampong@users.noreply.github.com>
2025-04-10 14:44:56 +0000

    Create main.cpp
    Main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+auto main()->int{
+  std::string username, password;
+  std::cout << "Username:";
+  std::cin >> username;
+  std::cout << "Password:";
+  std::cin >> password;
+
+  //authentication and authorization
+
+
+
+
+}
